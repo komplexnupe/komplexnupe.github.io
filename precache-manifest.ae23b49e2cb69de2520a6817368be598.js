487496dbd47b3e9cbdc612be4ae0a9c267b69508
Komplexnupe <leonard.love87@icloud.com>
2020-10-02 01:03:38 -0400

    Updates

---
+++ precache-manifest.ae23b49e2cb69de2520a6817368be598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2350ff5ad337cbdf01c3b5661c9b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8df771541d7e62ba4c3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "76b9072a24920ee3677b",
+    "url": "/static/css/main.22f82c3b.chunk.css"
+  },
+  {
+    "revision": "b8df771541d7e62ba4c3",
+    "url": "/static/js/2.6c386d9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c386d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b9072a24920ee3677b",
+    "url": "/static/js/main.3f99c8fd.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
