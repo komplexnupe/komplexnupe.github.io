5c037368ec6ccbabf55a1b5863b8d5abbaee9051
Komplexnupe <leonard.love87@icloud.com>
2020-10-02 02:42:10 -0400

    mini projects card update

---
+++ src/components/MiniProjects/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './style.css';
+
+const MiniProjects = (props) => {
+    return (
+        <Card className="col-3 card ml-5 mb-3" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.image} />
+            <Card.Body className="d-flex flex-column">
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>
+                    {props.technology}
+                </Card.Text>
+                <Button variant="danger" href={props.url} className="btn btn-danger mt-auto" target="_blank" rel="noopener noreferrer">View Demo</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default MiniProjects;
