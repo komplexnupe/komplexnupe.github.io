cacfb50ca5e8193fcbe0a95844610b1675b2c90f
Komplexnupe <leonard.love87@icloud.com>
2020-10-02 03:47:18 -0400

    Updates

---
+++ precache-manifest.83773c908daa6bf6ddcfc1da279f68e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4ae43928c84e9b5e33c4e0998af42c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "462b5cc980e468acba74",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0948412258ae662c483b",
+    "url": "/static/css/main.5026957c.chunk.css"
+  },
+  {
+    "revision": "462b5cc980e468acba74",
+    "url": "/static/js/2.e4bb0026.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e4bb0026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0948412258ae662c483b",
+    "url": "/static/js/main.4a906aa1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "996ea0aeac6a9f5bd15636dc18aa8d10",
+    "url": "/static/media/CockTellMe.996ea0ae.gif"
+  },
+  {
+    "revision": "d870a28b8c52412d0bddc7bdc71406f7",
+    "url": "/static/media/fitLinkWelcome.d870a28b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae23b49e2cb69de2520a6817368be598.js"
+  "/precache-manifest.83773c908daa6bf6ddcfc1da279f68e3.js"
 );
 
 self.addEventListener('message', (event) => {
