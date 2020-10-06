993588dfbd3196f1c1c98986842f7ced60a243c6
Komplexnupe <leonard.love87@icloud.com>
2020-10-05 23:48:36 -0400

    card image update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Bar from './components/Navbar';
 import TeamProjects from './components/TeamProjects';
 import MiniProjects from './components/MiniProjects';
-import miniprojects from './miniprojects.json';
 import Navbar from 'react-bootstrap/Navbar';
 import About from './components/About';
 
@@ -16,25 +15,50 @@ function App() {
       <About />
       <div className="container container-sm">
         <div className="row ml-5 mb-3">
-          {miniprojects.map(projects => {
-            return (
-              <MiniProjects
-                key={projects.id}
-                image={projects.image}
-                title={projects.title}
-                url={projects.url}
-                technology={projects.technology}
-              />
-            )
-          })}
+          <MiniProjects
+            image={require('./images/burgertime.png')}
+            title={"Burger Time"}
+            url={"https://leonardlove-burger-time.herokuapp.com/"}
+            technology={"Express.js, Handlebars.js, Heroku, MySQL, Node.js"}
+          />
+          <MiniProjects
+            image={require('./images/Employee-Directory.gif')}
+            title={"Employee Directory"}
+            url={"https://komplexnupe.github.io/ll-employee-directory/"}
+            technology={"React.js, JSX, Bootstrap, Node.js"}
+          />
+          <MiniProjects
+            image={require('./images/burgertime.png')}
+            title={"Weather Dashboard"}
+            url={"https://komplexnupe.github.io/5-day-weather-dashboard/"}
+            technology={"jQuery, Bootstrap, CSS, HTML5, Local Storage"}
+          />
+          <MiniProjects
+            image={require('./images/Fitness-Tracker.gif')}
+            title={"Fitness Tracker"}
+            url={"https://fitness-workout-tracker-22.herokuapp.com/"}
+            technology={"Mongo, Node.js, JavaScript, CSS, HTML5"}
+          />
+          <MiniProjects
+            image={require('./images/employeeTracker.gif')}
+            title={"Employee Dashboard"}
+            url={"https://github.com/komplexnupe/employee-tracker"}
+            technology={"Node.js, MySQL"}
+          />
+          <MiniProjects
+            image={require('./images/noteTaker.gif')}
+            title={"Note Taker"}
+            url={"https://leonardlove-note-taker.herokuapp.com/"}
+            technology={"Express.js, Handlebars.js, Heroku, MySQL, Node.js"}
+          />
         </div>
       </div>
-          <TeamProjects />
-    <Navbar id="nav" className="mt-3" sticky="bottom" bg="">
-    <Navbar.Brand className="text-white" href="#home">
-    “All our dreams can come true, if we have the courage to pursue them.” – Walt Disney.
+      <TeamProjects />
+      <Navbar id="nav" className="mt-3" sticky="bottom" bg="">
+        <Navbar.Brand className="text-white" href="#home">
+          “All our dreams can come true, if we have the courage to pursue them.” – Walt Disney.
     </Navbar.Brand>
-  </Navbar>
+      </Navbar>
     </div>
   );
 }
