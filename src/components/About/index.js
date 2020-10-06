de2a79a07a33eec7c2266634ec68db98bab7d77b
Komplexnupe <leonard.love87@icloud.com>
2020-10-05 23:52:04 -0400

    modal update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -41,7 +41,7 @@ const About = () => {
                     <Col xs={8} md={8} className="ml-3">
                         I am a Husband, a Father of 2 beautiful girls, a native Charlottean, and a former collegiate football player at Appalachian State where
                     I was on all 3 national championship teams.<br/><br/>
-                        Everyone has their "Why" when it comes to what led them down the path of engineering. As a non-developing founder, I invested a lot of money in outsourcing engineering for a MVP, which led me to ask myself..."why can't I code my own projects?"
+                        Everyone has their "Why" when it comes to what led them down the path of engineering. As a non-developing founder of a startup, I invested a lot of money in outsourcing engineering for a MVP, which led me to ask myself..."why can't I code my own projects?"
                         That question burned at me for a long time and made me want to dive under the hood and into the code to understand how I could learn how to build solutions for myself and others. That dive led me to UNCC's Boot Camp 
                         was the best decision I've ever made because I have found something that I truly love to do. Solving a problem or finding a bug is like intercepting a pass or scoring a touchdown! Having people use something that I built is equivalent
                         to having an entire stadium cheering for you. Simply put, I love to code.</Col></Row>
